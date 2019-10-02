98c0aec29e8c4f223d6dbe2c75f85a276a9cc485
Armand Pretorius <170045@virtualwindow.co.za>
2019-10-02 22:38:14 +0200

    Initial Commit

---
+++ Calcu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calcu
+//
+//  Created by Armand Pretorius on 2019/10/02.
+//  Copyright © 2019 Armand Pretorius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
